e9386bb03512946ef4c5af51eb4b2a5f5cce352c
Julien Metzelard <julien.metzelard@ensea.fr>
2025-10-15 09:12:09 +0200

    Added files from TD1

---
+++ TP1/Queue_Array.h
@@ -0,0 +1,8 @@
+//
+// Created by Julien Metzelard on 15/10/2025.
+//
+
+#ifndef INC_3A_TP_ALGO_QUEUE_ARRAY_H
+#define INC_3A_TP_ALGO_QUEUE_ARRAY_H
+
+#endif //INC_3A_TP_ALGO_QUEUE_ARRAY_H
--- TP1/main.cpp
+++ TP1/main.cpp
@@ -11,20 +11,20 @@ int main() {
     stack1.push(3);
     std::cout<<"Stack values Test : "<<stack1.pop()<<" "<<stack1.pop()<<" "<<stack1.pop()<<std::endl;
 
-
-    Stack_linked_list stack2 = Stack_linked_list<int>();
+*/
+    Stack_linked_list<int> stack2 = Stack_linked_list<int>();
     stack2.push(1);
     stack2.push(2);
     stack2.push(3);
     std::cout<<"Stack_linked_list values Test : "<<stack2.pop()<<" "<<stack2.pop()<<" "<<stack2.pop()<<std::endl;
 
-*/
-    Queue queue = Queue<int>();
+/*
+    Queue<int> queue = Queue<int>();
     queue.push(1);
     queue.push(2);
     queue.push(3);
     std::cout<<"Queue values Test : "<<queue.pop()<<" "<<queue.pop()<<" "<<queue.pop()<<std::endl;
-
+*/
 
 
     return 0;
