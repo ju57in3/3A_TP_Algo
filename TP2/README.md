2bc7d6108a7b15608b86fa767de724de0aae80dc
ju57in3 <justine.hazan@ensea.fr>
2025-10-22 12:00:02 +0200

    Exo1

---
+++ TP2/README.md
@@ -0,0 +1,69 @@
+# 3A_TP_Algo
+## TP2
+### Exercice 1
+#### Affichage
+
+----------------------------------------------------
+**Reading the input file and displaying the array :**  
+Array size : 11  
+Elements in the array : 1 2 3 8 9 4 5 6 20 9 10
+----------------------------------------------------
+**Length of the longest increasing subsequence :**  
+Length = 8
+----------------------------------------------------
+**Complete increasing subsequence max :**  
+Length : 8  
+Increasing subsequence : 1 2 3 4 5 6 9 10
+----------------------------------------------------
+
+### Exercice 2
+
+### Exercice 3
+#### Affichage
+
+-----------------------------------------------------
+**Reading the file and displaying the graph :**  
+Number of vertices : 11  
+Number of edges : 14  
+List of edges :  
+1 2  
+1 3  
+2 3  
+2 4  
+2 5  
+3 6  
+3 7  
+4 8  
+5 8  
+5 9  
+6 10  
+7 11  
+10 11  
+7 11
+-----------------------------------------------------
+**Spanning tree with adjacence matrix :**  
+1 2  
+1 3  
+2 4  
+2 5  
+3 6  
+3 7  
+4 8  
+5 9  
+6 10  
+7 11
+-----------------------------------------------------
+**Spanning tree with adjacence list :**  
+1 2  
+1 3  
+2 4  
+2 5  
+3 6  
+3 7  
+4 8  
+5 9  
+6 10  
+7 11
+-----------------------------------------------------
+
+
--- TP2/exo1.cpp
+++ TP2/exo1.cpp
@@ -2,4 +2,106 @@
 // Created by justine on 22/10/2025.
 //
 #include <iostream>
+#include <fstream>
 #include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void read_display_array() {
+    ifstream in("/home/justine/CLionProjects/3A_TP_Algo/TP2/INPMONOSEQ.TXT");
+    if (!in) {
+        cout << "Error opening file" << endl;
+        return;
+    }
+    int n;
+    in >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) in >> a[i];
+
+    cout << "Array size : " << n << endl;
+    cout << "Elements in the array : ";
+    for (int x : a) cout << x << " ";
+    cout << endl;
+}
+
+void longest_increasing_subseq() {
+    ifstream in("/home/justine/CLionProjects/3A_TP_Algo/TP2/INPMONOSEQ.TXT");
+    ofstream out("/home/justine/CLionProjects/3A_TP_Algo/TP2/OUTMONOSEQ.TXT");
+    if (!in || !out) {
+        cout << "Error opening file" << endl;
+        return;
+    }
+
+    int n;
+    in >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) in >> a[i];
+
+    vector<int> L(n, 1);
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < i; j++)
+            if (a[i] > a[j])
+                L[i] = max(L[i], L[j] + 1);
+
+    int length = *max_element(L.begin(), L.end());
+    cout << "Length = " << length << endl;
+    out << length << endl;
+}
+
+void complete_max_increasing_subseq() {
+    ifstream in("/home/justine/CLionProjects/3A_TP_Algo/TP2/INPMONOSEQ.TXT");
+    ofstream out("/home/justine/CLionProjects/3A_TP_Algo/TP2/OUTMONOSEQ.TXT");
+    if (!in || !out) {
+        cout << "Error opening file" << endl;
+        return;
+    }
+
+    int n;
+    in >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) in >> a[i];
+
+    vector<int> L(n, 1), pred(n, -1);
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < i; j++)
+            if (a[i] > a[j] && L[j] + 1 > L[i]) {
+                L[i] = L[j] + 1;
+                pred[i] = j;
+            }
+
+    int maxLen = 0, last = 0;
+    for (int i = 0; i <n; i++)
+        if (L[i] > maxLen) {
+            maxLen = L[i];
+            last = i;
+        }
+
+    vector<int> seq;
+    for (int i = last; i != -1; i = pred[i]) seq.push_back(i);
+    reverse(seq.begin(), seq.end());
+
+    cout << "Length : " << maxLen << endl;
+    out << maxLen << endl;
+    cout << "Increasing subsequence : ";
+    for (int idx : seq) {
+        cout << a[idx] << " ";
+        out << "a[" << idx + 1 << "] = " << a[idx] << "\n";
+    }
+    cout << endl;
+}
+
+int main() {
+    cout << " EXERCICE 1" << endl;
+    cout << "----------------------------------------------------" << endl;
+    cout << "Reading the input file and displaying the array :" << endl;
+    read_display_array();
+    cout << "----------------------------------------------------" << endl;
+    cout << "Length of the longest increasing subsequence :" << endl;
+    longest_increasing_subseq();
+    cout << "----------------------------------------------------" << endl;
+    cout << "Complete increasing subsequence max:" << endl;
+    complete_max_increasing_subseq();
+    cout << "----------------------------------------------------" << endl;
+    return 0;
+}
